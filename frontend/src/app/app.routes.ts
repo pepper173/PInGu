f434f449fcbe1e686654e57288a63322054f01df
Leo <test@mail.de>
2025-12-17 00:33:14 +0100

    Remove student sign up.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,15 +3,13 @@ import {authGuard} from './auth/auth.guard';
 import { StudentLoginHome } from './components/studentLogin/student-login-home/student-login-home';
 import { teacherLogin } from './components/teacherLogin/teacherLogin';
 import { TeacherOverviewComponent } from './components/teacherView/teacherOverview';
-import { StudentSignupComponent } from './components/studentSignUp/student-singup';
 import {TeacherSignupComponent} from './components/teacherSignup/signup';
 
 export const routes: Routes = [
   { path: 'studentLogin', component: StudentLoginHome },
   { path: 'teacherLogin', component: teacherLogin},
-  { path: 'teacherOverview', component: TeacherOverviewComponent, canMatch: [authGuard]},
-  { path: 'studentSignup', component: StudentSignupComponent},
   { path: 'teacherSignup', component: TeacherSignupComponent},
+  { path: 'teacherOverview', component: TeacherOverviewComponent, canMatch: [authGuard]},
   { path: '', pathMatch: 'full', redirectTo: 'teacherLogin' },
   { path: '**', redirectTo: 'teacherLogin' },
   {
